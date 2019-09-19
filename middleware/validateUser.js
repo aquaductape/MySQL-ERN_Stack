fc36f0d1227c77acdcf9bc6c6bf782bc04663217
Caleb <aquaductape@gmail.com>
2019-09-19 11:18:01 -0700

    updated validation

--- middleware/validateUser.js
+++ middleware/validateUser.js
@@ -1,22 +1,18 @@
 const { check } = require('express-validator');
 
 module.exports = {
-  login() {
-    return [
-      check('email', 'Please enter a valid email').isEmail(),
-      check('password', 'Password is required').exists(),
-    ];
-  },
-  signUp() {
-    return [
-      check('name', 'Name is required')
-        .not()
-        .isEmpty(),
-      check('email', 'Please enter a valid email').isEmail(),
-      check(
-        'password',
-        'Please enter a password with 6 or more characters'
-      ).isLength({ min: 6 }),
-    ];
-  },
+  login: [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  signUp: [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
 };
