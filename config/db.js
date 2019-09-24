94bf03015bd5b5979a42a47aed9019fa3e76a7fa
Caleb <aquaductape@gmail.com>
2019-09-23 18:34:21 -0700

    add database class to query

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const config = require('config');
 const mysql = require('mysql');
 
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit: 10,
   host: config.get('db.host'),
   port: config.get('db.port'),
@@ -11,13 +11,101 @@ const connection = mysql.createPool({
   multipleStatements: true,
 });
 
-connection.getConnection(async err => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.log(err);
-    return err;
+    throw err;
   }
-
-  console.log('Connected to mysql 🐬 ');
+  console.log('MySQL connected... 🐬 ');
+  // Create tables
+  require('../models/User');
+  require('../models/Profile');
+  require('../models/Post');
+  require('../models/Education');
+  require('../models/Experience');
+  require('../models/Likes');
+  require('../models/Comments');
+  require('../models/Social');
+  require('../models/Skills');
+  connection.release();
 });
 
-module.exports = connection;
+class Database {
+  constructor(pool) {
+    this.pool = pool;
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.pool.getConnection((err, connection) => {
+        if (err) return reject(err);
+
+        connection.query(sql, args, (err, rows, fields) => {
+          connection.release();
+          if (err) return reject(err);
+
+          // If query to select all columns from a table then return as an array
+          // If query is modifying
+          if (!sql.match(/where/gi)) {
+            resolve(rows);
+          }
+          // If query is to pick a column, return null if it doesn't exist
+          if (rows.length === 0) {
+            resolve(null);
+          }
+
+          if (rows.length > 1) {
+            // If rows contains an OkPacket, get the second item which contains rows
+            // this is usefull when selecting the column after modifying it
+            if (rows[0].constructor.name === 'OkPacket') {
+              resolve(rows[1]);
+            }
+            resolve(rows);
+          }
+
+          resolve(rows[0]);
+        });
+      });
+    });
+  }
+
+  createTable(name, sql, args) {
+    const tableExist = rows => {
+      for (let row of rows) {
+        if (row.Code === 1050) return true;
+        if (Array.isArray(row)) return tableExist(row);
+      }
+    };
+    this.pool.getConnection((err, connection) => {
+      if (err) return reject(err);
+
+      connection.query(sql, args, (err, rows) => {
+        // console.log('TCL: Database -> createTable -> rows', rows);
+        // console.log('ran');
+        connection.release();
+        if (err) {
+          console.log(err);
+          throw err;
+        }
+        if (!tableExist(rows)) {
+          console.log(`Created ${name} TABLE`);
+        }
+      });
+    });
+  }
+
+  createTrigger() {
+    // Mysql: ...if you use the mysql program to define a trigger that executes multiple statements, it is necessary to redefine the mysql statement DELIMITER so that you can use the ; statement DELIMITER within the trigger definition.
+
+    // this mysql nodejs driver is throwing syntax errors by using DELIMITER, probably because multiple queries are enabled, therefore DELIMITERs are not included
+    const queryString = `
+        DROP TRIGGER IF EXISTS ${triggerName};
+        CREATE TRIGGER ${triggerName}
+        BEFORE ${type} ON ${name} FOR EACH ROW
+        BEGIN
+          ${blockEmptyString}
+        END;
+        `;
+  }
+}
+
+module.exports = db = new Database(pool);
